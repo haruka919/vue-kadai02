ec50bac83b96a20d2b4bdcfe2bbebce8271a43cc
parukoo <hrk.0u0x@gmail.com>
2020-08-10 15:08:05 +0900

    生年月日のフォームをvueに書き換え

--- src/views/FormProfile.vue
+++ src/views/FormProfile.vue
@@ -13,10 +13,10 @@
               <label class="has-text-link">-性別-</label>
               <div class="control mt-2 has-text-black">
                 <label class="radio">
-                  <input type="radio" class="mr-1" name="question">男性
+                  <input type="radio" class="mr-1" name="gender" value="male" v-model="picked">男性
                 </label>
                   <label class="radio">
-                  <input type="radio" class="mr-1" name="question">女性
+                  <input type="radio" class="mr-1" name="gender" value="female" v-model="picked">女性
                 </label>
               </div>
             </div>
@@ -24,23 +24,20 @@
               <label class="has-text-link">-生年月日-</label>
               <div class="control is-flex mt-2 has-text-black">
                 <div class="select">
-                  <select>
-                    <option>1990（平成1）</option>
-                    <option>1991（平成2）</option>
+                  <select v-model="selectedYear">
+                    <option v-for="y in years" :key="y" :value="y">{{ showYear(y) }}</option>
                   </select>
                 </div>
                 <div class="mx-1 mt-3">年</div>
                 <div class="select">
-                  <select>
-                    <option>1990</option>
-                    <option>1991</option>
+                  <select v-model="selectedMonth">
+                    <option v-for="month in 12" :key="month" :value="month">{{ month }}</option>
                   </select>
                 </div>
                 <div class="mx-1 mt-3">月</div>
                 <div class="select">
-                  <select>
-                    <option>1990</option>
-                    <option>1991</option>
+                  <select v-model="selectedDay">
+                    <option v-for="day in maxDays" :key="day" :value="day">{{ day }}</option>
                   </select>
                 </div>
                 <div class="mx-1 mt-3">日</div>
@@ -60,6 +57,43 @@
 
 <script>
 export default {
-  name: 'FormProfile'
+  name: 'FormProfile',
+  data () {
+    return {
+      selectedYear: 1990,
+      selectedMonth: 1,
+      selectedDay: 1,
+      years: [...Array(50)].map((_, i) => i + 1955),
+      maxDays: ''
+    }
+  },
+  watch: {
+    selectedYear () {
+      return this.getMaxDays()
+    },
+    selectedMonth () {
+      return this.getMaxDays()
+    }
+  },
+  methods: {
+    // 指定年月の最終日を取得
+    getMaxDays () {
+      this.maxDays = new Date(this.selectedYear, this.selectedMonth, 0).getDate()
+    },
+    showYear (year) {
+      if (year >= 2019) {
+        return year + '（令和' + (year - 2018) + '年）'
+      }
+      if (year < 2019 && year >= 1989) {
+        return year + '（平成' + (year - 1988) + '年）'
+      }
+      if (year < 1989 && year >= 1926) {
+        return year + '（昭和' + (year - 1925) + '年）'
+      }
+    }
+  },
+  created () {
+    this.getMaxDays()
+  }
 }
 </script>
